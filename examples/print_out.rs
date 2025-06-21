3ab962930c7eebd2eab5998177bd6091e6b94e73
EyalZusiman <eyal.zusiman@gmail.com>
2025-06-22 00:54:41 +0300

    make print_out output to a file

--- examples/print_out.rs
+++ examples/print_out.rs
@@ -1,18 +1,28 @@
 use bhlang::LangFile;
 use std::error::Error;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, BufWriter, Write};
+
+const LANG_PATH: &str =
+    "C:/Program Files (x86)/Steam/steamapps/common/Brawlhalla/languages/language.1.bin";
+const OUT_PATH: &str =
+    "C:/Program Files (x86)/Steam/steamapps/common/Brawlhalla/languages/language.1.tsv";
 
 fn main() -> Result<(), Box<dyn Error>> {
-    const LANG_PATH: &str =
-        "C:/Program Files (x86)/Steam/steamapps/common/Brawlhalla/languages/language.1.bin";
-
     let file = File::open(LANG_PATH)?;
     let reader = BufReader::new(file);
     let lang_file = LangFile::read(reader)?;
 
-    for (key, value) in lang_file.iter() {
-        println!("{key} = {value}");
+    let outfile = File::create(OUT_PATH)?;
+    let mut writer = BufWriter::new(outfile);
+
+    let mut entries: Vec<_> = lang_file.iter().collect();
+    entries.sort();
+    for (key, value) in entries {
+        // replace special chars
+        let cleanvalue = value.replace('\n', "\\n").replace('\r', "\\r");
+
+        writer.write(format!("{key}\t{cleanvalue}\n").as_bytes())?;
     }
 
     Ok(())
